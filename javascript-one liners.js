c10c372c1989faa5c4ca69d4e4d4a88670f83cde
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-26 01:12:10 +0530

    Update javascript-one liners.js

--- javascript-one liners.js
+++ javascript-one liners.js
@@ -14,47 +14,47 @@
   new Date().toString()
 //--------------------------------------
 6)Check if a variable is an array:
-Array.isArray(variable)
+  Array.isArray(variable)
 //--------------------------------------
 7)Check if a variable is an object:
-typeof variable === "object"
+  typeof variable === "object"
 //--------------------------------------
 8)Convert an array to a string:
-array.join(",")
+  array.join(",")
 //--------------------------------------
 9)Check if a variable is a function:
-typeof variable === "function"
+  typeof variable === "function"
 //--------------------------------------
 10)Convert an object to an array:
-Object.values(object)
+  Object.values(object)
 //--------------------------------------
 11)Count the occurrences of an element in an array:
-array.filter(x => x === element).length
+  array.filter(x => x === element).length
 //--------------------------------------
 12)Create a new object with a dynamic key and value:
-{ [key]: value }
+  { [key]: value }
 //--------------------------------------
 13)Check if a string is a palindrome:
-string === string.split("").reverse().join("")
+  string === string.split("").reverse().join("")
 //--------------------------------------
 14)Get the the sum of all the numbers in an array
-array.reduce((a, b) => a + b, 0));
+  array.reduce((a, b) => a + b, 0));
 //--------------------------------------
 15) Get the current timestamp:
-Date.now()
+  Date.now()
 //--------------------------------------
 16)Check if a variable is null:
-variable === null
+  variable === null
 //--------------------------------------
 17)Check if a variable is undefined:
-typeof variable === "undefined"
+  typeof variable === "undefined"
 //--------------------------------------
 18)Find the minimum value in an array
-Math.min(...array)
+  Math.min(...array)
 //--------------------------------------
 19) Check if an array is empty:
-array.length === 0
+  array.length === 0
 //--------------------------------------
 20)Create a new array with a specified range of numbers:
-Array.from({ length: n }, (_, i) => i)
+  Array.from({ length: n }, (_, i) => i)
 
