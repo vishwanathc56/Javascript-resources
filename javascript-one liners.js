36b2c4540907c384d4448f13e60c29023aef642c
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-26 01:08:34 +0530

    Update javascript-one liners.js

--- javascript-one liners.js
+++ javascript-one liners.js
@@ -1,1 +1,31 @@
-
+1) Find the max value in an array:
+  Math.max(...array)
+//--------------------------------------
+2) Remove duplicates from an array:
+  [...new Set(array)]
+//--------------------------------------
+3)Generate a random number between 1 and 100:
+  Math.floor(Math.random() * 100) + 1
+//--------------------------------------
+4)Check if a string is a valid number:
+  !isNaN(parseFloat(string))
+//--------------------------------------
+2)Get the current date and time:
+  new Date().toString()
+//--------------------------------------
+2)Check if a variable is an array:
+Array.isArray(variable)
+//--------------------------------------
+2)Check if a variable is an object:
+typeof variable === "object"
+//--------------------------------------
+2)Convert an array to a string:
+array.join(",")
+//--------------------------------------
+2)Check if a variable is a function:
+typeof variable === "function"
+//--------------------------------------
+2)Convert an object to an array:
+Object.values(object)
+//--------------------------------------
+2)
