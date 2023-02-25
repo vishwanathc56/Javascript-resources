aeca35c07abde47173d4d92d4580f908de35280d
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-26 01:10:36 +0530

    Update javascript-one liners.js

--- javascript-one liners.js
+++ javascript-one liners.js
@@ -28,4 +28,33 @@ typeof variable === "function"
 2)Convert an object to an array:
 Object.values(object)
 //--------------------------------------
-2)
+2)Count the occurrences of an element in an array:
+array.filter(x => x === element).length
+//--------------------------------------
+2)Create a new object with a dynamic key and value:
+{ [key]: value }
+//--------------------------------------
+2)Check if a string is a palindrome:
+string === string.split("").reverse().join("")
+//--------------------------------------
+2)Get the the sum of all the numbers in an array
+array.reduce((a, b) => a + b, 0));
+//--------------------------------------
+2) Get the current timestamp:
+Date.now()
+//--------------------------------------
+2)Check if a variable is null:
+variable === null
+//--------------------------------------
+2)Check if a variable is undefined:
+typeof variable === "undefined"
+//--------------------------------------
+2)Find the minimum value in an array
+Math.min(...array)
+//--------------------------------------
+2) Check if an array is empty:
+array.length === 0
+//--------------------------------------
+2)Create a new array with a specified range of numbers:
+Array.from({ length: n }, (_, i) => i)
+
