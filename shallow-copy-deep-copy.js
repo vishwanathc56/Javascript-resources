39e13d01db76dd49059d5d453c0d89fef564ef3a
Vishwanath C <meetvishwa123@rediffmail.com>
2023-03-10 21:35:52 +0530

    Update shallow-copy-deep-copy.js

--- shallow-copy-deep-copy.js
+++ shallow-copy-deep-copy.js
@@ -1,1 +1,21 @@
+1)//shallow cloning vs deep cloning
+
+let original = { a: 1, b: { c: 2 } };
+
+let shallowCopy = {...original}
+//console.log(shallowCopy)  { a: 1, b: { c: 2 } }
+
+let deepCopy = JSON.parse(JSON.stringify(original));
+//console.log(deepCopy)     { a: 1, b: { c: 2 } }
+
+original.b.c = 3;
+
+console.log(original.b.c)
+
+console.log(shallowCopy.b.c)
+console.log(deepCopy.b.c)
+
+// ans :  3,3,2
+
+
 
