3adcd02072f2d052c3acc09a21e836f7d487f1ae
Vishwanath C <meetvishwa123@rediffmail.com>
2023-03-10 22:28:41 +0530

    Update shallow-copy-deep-copy.js

--- shallow-copy-deep-copy.js
+++ shallow-copy-deep-copy.js
@@ -17,5 +17,31 @@ console.log(deepCopy.b.c)
 
 // ans :  3,3,2
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+// Given an array of objects, write a function that creates a shallow copy and a deep copy of the array.
+
+  const originalArray = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 35 },
+  ];
+
+  function copyArray(originalArray) {
+    // Create a shallow copy of the original array using the slice() method.
+    const shallowCopy = originalArray.slice();
+
+    // Create a deep copy of the original array using the JSON.parse() and JSON.stringify() methods.
+    const deepCopy = JSON.parse(JSON.stringify(originalArray));
+
+    return [shallowCopy, deepCopy];
+  }
+
+  // Test the function
+  const [shallowCopy, deepCopy] = copyArray(originalArray);
+
+  console.log(shallowCopy[0] === originalArray[0]); // true
+  console.log(deepCopy[0] === originalArray[0]); // false
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
