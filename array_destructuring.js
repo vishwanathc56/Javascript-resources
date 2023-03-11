4eb22a56352e25dcf6124447a3ab4d70484f7d1b
Vishwanath C <meetvishwa123@rediffmail.com>
2023-03-12 01:03:12 +0530

    Update array_destructuring.js

--- array_destructuring.js
+++ array_destructuring.js
@@ -16,67 +16,68 @@ const animals = [
 ]
 
 // Basic Array
-/* const [animal1] = animals;
-console.log(animal1); { name: 'Dog', Legs: 4 } */
+const [animal1] = animals;
+console.log(animal1); { name: 'Dog', Legs: 4 } 
 
-/* const [animal1, cat] = animals;
-console.log(cat); { name: 'Cat', Legs: 40 } */
+const [animal1, cat] = animals;
+console.log(cat); { name: 'Cat', Legs: 40 } 
 
 // Array with default
-/* const [dog, cat, fish, bird = 'Jon'] = animals;
-console.log(bird); Jon */
+const [dog, cat, fish, bird = 'Jon'] = animals;
+console.log(bird); Jon
 
 // Array with ignore    
-/* const [, cat] = animals;
+const [, cat] = animals;
 console.log(cat); { name: 'Cat', Legs: 40 }
 
 const [, , fish] = animals;
 console.log(fish); { name: 'Fish', Legs: 0 }
- */
+
 //Array with rest
-/* const [dog, ...rest] = animals;
+const [dog, ...rest] = animals;
 console.log(rest);[{ name: 'Cat', Legs: 40 }, { name: 'Fish', Legs: 0 }]
 
 const [, cat, ...rest] = animals;
-console.log(rest);[{ name: 'Fish', Legs: 0 }] */
+console.log(rest);[{ name: 'Fish', Legs: 0 }] 
 
 // Function
-/* function greeting(args) {
+function greeting(args) {
     return console.log(`He is ${args.name} who is ${args.age} old`);
 }
- */
+
 //  greeting({name:"jon", age:55}); // He is jon who is 55 old
 
-/* function greeting({ name, age }) {
+function greeting({ name, age }) {
     return console.log(`He is ${name} who is ${age} old`);
-} */
+} 
 
 // greeting({name:"jon", age:55}); // He is jon who is 55 old
 
-/* function greeting({ name = 'jane', age = 50 } = {}) {
+function greeting({ name = 'jane', age = 50 } = {}) {
     return console.log(`He is ${name} who is ${age} old`);
-} */
+} 
 
-// greeting(); // He is jane who is 50 old
+ greeting(); // He is jane who is 50 old
 
-// greeting({name:"jon"}); //He is jon who is 50 old
+ greeting({name:"jon"}); //He is jon who is 50 old
+
+
 // Function rest
-
-/* function greeting({ name, ...rest }) {
+function greeting({ name, ...rest }) {
     return console.log(`He is ${name} who is ${rest} old`);
-} */
+} 
 
 // greeting({name:"jon", age:55, country: 'India'}); //He is jon who is [object Object] old
 
-/* function greeting({ name, ...rest }) {
+function greeting({ name, ...rest }) {
     return console.log(`He is ${name} who is ${rest.age} old`);
-} */
+} 
 
 // greeting({name:"jon", age:55, country: 'India'}); //He is jon who is 55 old
 
-/* function greeting({ country, ...rest }) {
+function greeting({ country, ...rest }) {
     return console.log(`He is ${country} who is ${rest.age} old`);
-} */
+} 
 
 greeting({ name: "jon", age: 55, country: 'India' }); //He is India who is 55 old
 
