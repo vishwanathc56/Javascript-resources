908da2145fbda13cd5d2eaec84e86a0e5c739512
Vishwanath C <meetvishwa123@rediffmail.com>
2023-03-21 23:29:39 +0530

    Update type-checking.js

--- type-checking.js
+++ type-checking.js
@@ -3,3 +3,5 @@ console.log(typeof undefined);            //undefined
 console.log(null === undefined);          //false
 console.log(null == undefined);           //true
 //-----------------------------------------------------------------------------------------------------------------------------------------------
+console.log(typeof class {});             //function
+
