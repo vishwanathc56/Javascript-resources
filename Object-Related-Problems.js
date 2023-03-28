d6ae4e16294d2632abf968b4b7b1d3ec5f663abc
Vishwanath C <meetvishwa123@rediffmail.com>
2023-03-28 23:13:03 +0530

    Update Object-Related-Problems.js

--- Object-Related-Problems.js
+++ Object-Related-Problems.js
@@ -6,3 +6,16 @@ console.log(result);                  // LiveScript
 Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object. 
 The Object.assign() method only copies enumerable and own properties from a source object to a target object. It uses [[Get]] on the source and [[Set]] on the target, so it will invoke getters and setters. Therefore it assigns properties, versus copying or defining new properties. This may make it unsuitable for merging new properties into a prototype if the merge sources contain getters.
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+2) let x ={
+    y: 'z',
+    print:()=> {
+       return this.y ==="z"; 
+    }
+}
+
+console.log(x.print());  
+
+Answer: false
+Reason: There is no binding of 'this' with arrow function
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
