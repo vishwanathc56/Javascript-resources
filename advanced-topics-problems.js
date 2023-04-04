1a69bc0020f33cf375a46bee83a1bf79206e80b8
Vishwanath C <meetvishwa123@rediffmail.com>
2023-04-04 22:44:26 +0530

    Update advanced-topics-problems.js

--- advanced-topics-problems.js
+++ advanced-topics-problems.js
@@ -14,3 +14,13 @@ console.log(mul(2)(3)[1](4));
 
 ans : 6 24
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
+(() => {
+let x = (y = 10);
+})();
+console.log(typeof y);
+
+ans : number
+
+Result is a number. However, if it is in strict mode ( 'use strict' ), then it will throw a reference error. 
+It doesn't allow implicit global variable declarations when it is in strict mode.
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
