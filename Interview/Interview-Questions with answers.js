d2c7d657b9930b655a81ae347a63bdf79bc83ce5
Vishwanath C <meetvishwa123@rediffmail.com>
2023-04-08 20:34:41 +0530

    Update Interview-Questions with answers.js

--- Interview/Interview-Questions with answers.js
+++ Interview/Interview-Questions with answers.js
@@ -1,1 +1,8 @@
-
+//----------------------------------------------------------------------------------------------------------------------------------------------
+https://stackoverflow.com/questions/3397161/should-i-use-document-createdocumentfragment-or-document-createelement
+//----------------------------------------------------------------------------------------------------------------------------------------------
+Q: Should I use document.createDocumentFragment or document.createElement ?
+  
+ Ans : The difference is that a document fragment effectively disappears when you add it to the DOM. What happens is that all the child nodes of the document fragment are inserted at the location in the DOM where you insert the document fragment and the document fragment itself is not inserted. 
+ The fragment itself continues to exist but now has no children.
+ //----------------------------------------------------------------------------------------------------------------------------------------------
