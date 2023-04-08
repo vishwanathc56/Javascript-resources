21921705ea20eb1e6a2b6246f4df227af258e565
Vishwanath C <meetvishwa123@rediffmail.com>
2023-04-08 20:36:32 +0530

    Update Interview-Questions with answers.js

--- Interview/Interview-Questions with answers.js
+++ Interview/Interview-Questions with answers.js
@@ -6,3 +6,9 @@ Q: Should I use document.createDocumentFragment or document.createElement ?
  Ans : The difference is that a document fragment effectively disappears when you add it to the DOM. What happens is that all the child nodes of the document fragment are inserted at the location in the DOM where you insert the document fragment and the document fragment itself is not inserted. 
  The fragment itself continues to exist but now has no children.
  //----------------------------------------------------------------------------------------------------------------------------------------------
+
+ Q : What are differences between scope chaina and closure ?
+   
+ Q : What are differences between Regular Function & Arrow Function inJavascript ?
+   
+ 
