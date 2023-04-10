ffa22cae385e3571a840854ae68a62985ef8b55f
Vishwanath C <meetvishwa123@rediffmail.com>
2023-04-10 23:20:07 +0530

    Update Common-Interview-QnA.js

--- Interview/Common-Interview-QnA.js
+++ Interview/Common-Interview-QnA.js
@@ -1,1 +1,7 @@
-
+// 1)  Var vs let vs Const
+// 2) Prototype & how it works 
+//    Diff between regular func vs arrow func 
+//    why react has 1way data binding vs angular 2 way data binding 
+//    drawbacks of react
+//    pure component and examples 
+//    display : none vs visibility : hidden 
