5381d7b1a7d355a775ee75e7e70ad494d0bc16a0
Vishwanath C <meetvishwa123@rediffmail.com>
2023-04-18 01:43:38 +0530

    Update ocding-probs-solns.js

--- learners-bucket/ocding-probs-solns.js
+++ learners-bucket/ocding-probs-solns.js
@@ -1,1 +1,21 @@
+//https://learnersbucket.com/examples/interview/filter-array-of-objects-on-value-or-index/
+//Implement a function in JavaScript that filters an array of objects based on the value or index.
 
+Input:
+const arr = [
+  { name: "Amir", id: "1" },
+  { name: "Samlan", id: "2" },
+  { name: "Shahrukh", id: "0" },
+];
+
+console.log(filterObject(arr, 0)); // { name: "Amir", id: "1" }
+console.log(filterObject(arr, "Amir")); // { name: "Amir", id: "1" }
+console.log(filterObject(arr, "0")); // { name: "Shahrukh", id: "0" }
+
+ans : const filterone = (arr, id) => {
+        return arr.filter((item) => {
+          return item.id == id;
+        });
+      };  // [ { name: 'Shahrukh', id: '0' } ]
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
