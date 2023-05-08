2876a039c512e497e2b29b4a1cb861caffbefc2e
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-08 23:05:01 +0530

    Update find.js

--- Polyfills/find.js
+++ Polyfills/find.js
@@ -1,1 +1,19 @@
 
+if (!Array.prototype.find) {
+  Array.prototype.find = function(callback) {
+    for (var i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return this[i];
+      }
+    }
+    return undefined;
+  };
+}
+
+var arr = [1, 2, 3, 4, 5];
+
+var found = arr.find(function(element) {
+  return element > 3;
+});
+
+console.log(found); // 4
