3d7afddb0ff0c2cd07e7c1fc8bd141ac29a739e6
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-09 00:11:08 +0530

    Update find.js

--- Polyfills/find.js
+++ Polyfills/find.js
@@ -1,11 +1,22 @@
 
+// Check if Array.prototype.find exists
 if (!Array.prototype.find) {
+
+  // If it does not exist, add it to the prototype
   Array.prototype.find = function(callback) {
+
+    // Loop through each element of the array
     for (var i = 0; i < this.length; i++) {
+
+      // Call the callback function for each element with the element, index, and array as arguments
       if (callback(this[i], i, this)) {
+
+        // If the callback function returns true, return the current element
         return this[i];
       }
     }
+
+    // If no element is found, return undefined
     return undefined;
   };
 }
