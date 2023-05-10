c64c587fdc455a09db5647a61c8b4100f7fd0359
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-10 23:19:05 +0530

    Create push.js

---
+++ Polyfills/push.js
@@ -0,0 +1,15 @@
+if (!Array.prototype.push) {
+  Array.prototype.push = function(...items) {
+    const len = this.length;
+    for (let i = 0; i < items.length; i++) {
+      this[len + i] = items[i];
+    }
+    return this.length;
+  };
+}
+const array = [1, 2, 3];
+
+array.push(4, 5);
+
+// prints [1,2,3,4,5]
+console.log(array);
