a3c20e72053140a516c8446b8fc479e2e8357783
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-22 22:52:02 +0530

    Update includes.js

--- Polyfills/includes.js
+++ Polyfills/includes.js
@@ -1,1 +1,11 @@
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return true;
+      }
+    }
+    return false;
+  };
+}
 
