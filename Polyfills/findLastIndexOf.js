a7e3666d37bb5e758014cee401617a13108e0203
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-22 23:00:31 +0530

    Update findLastIndexOf.js

--- Polyfills/findLastIndexOf.js
+++ Polyfills/findLastIndexOf.js
@@ -1,1 +1,30 @@
 
+if (!Array.prototype.findLastIndex) {
+  Array.prototype.findLastIndex = function (predicate, thisArg) {
+    if (this === undefined || this === null) {
+      throw new TypeError('Cannot convert this value to object');
+    }
+
+    var array = Object(this);
+    var length = array.length >>> 0;
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+
+    for (var i = length - 1; i >= 0; i--) {
+      if (i in array && predicate.call(thisArg, array[i], i, array)) {
+        return i;
+      }
+    }
+
+    return -1;
+  };
+}
+
+const array = [1,2,3,12,13,8];
+
+const greaterThanTen = (element) => element > 10;
+
+console.log(array.findLastIndex(greaterThanTen)); //output: 4
+
+
