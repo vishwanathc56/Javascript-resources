7273882fc03ab7218671e4c9fafce30fe1edca1f
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-25 21:54:26 +0530

    Update findLast.js

--- Polyfills/findLast.js
+++ Polyfills/findLast.js
@@ -1,1 +1,24 @@
 
+if (!Array.prototype.findLast) {
+  Array.prototype.findLast = function(predicate) {
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+
+    for (let i = this.length - 1; i >= 0; i--) {
+      if (predicate(this[i], i, this)) {
+        return this[i];
+      }
+    }
+
+    return undefined;
+  };
+}
+
+
+const array = [1,2,3,12,13,8];
+
+const greaterThanTen = (element) => element > 10;
+
+console.log(array.findLast(greaterThanTen));
+// expected output: 13
