4a3c374ed4fb8fba9c446052fcd65ddfa3f7d38e
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-25 22:41:16 +0530

    Update indexOf.js

--- Polyfills/indexOf.js
+++ Polyfills/indexOf.js
@@ -1,1 +1,25 @@
 
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement, fromIndex = 0) {
+    const length = this.length;
+    
+    // Handle negative starting index
+    let startIndex = fromIndex >= 0 ? fromIndex : Math.max(0, length + fromIndex);
+    
+    for (let i = startIndex; i < length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+    
+    return -1; // Element not found
+  };
+}
+
+
+var array = [2, 9, 9];
+console.log(array.indexOf(2));     // 0
+console.log(array.indexOf(7));     // -1
+console.log(array.indexOf(9, 2));  // 2
+console.log(array.indexOf(2, -1)); // -1
+console.log(array.indexOf(2, -3)); // 0
