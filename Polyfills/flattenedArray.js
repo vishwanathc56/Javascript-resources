45f5f47764a15fafc37e1cbbe84c5ed020000008
Vishwanath C <meetvishwa123@rediffmail.com>
2023-05-25 22:47:31 +0530

    Create flattenedArray.js

---
+++ Polyfills/flattenedArray.js
@@ -0,0 +1,27 @@
+function flatten(arr) {
+  let flattened = [];
+  
+  arr.forEach(element => {
+    if (Array.isArray(element)) {
+      flattened = flattened.concat(flatten(element));
+    } else {
+      flattened.push(element);
+    }
+  });
+  
+  return flattened;
+}
+
+var input = [
+  1, 2, 3,
+   [4],
+  [5, 6, [7], [8, [9, [10]]]],
+  11, 12, 13,
+  [14, [[[[[15, [16]]]]]]],
+  17, 18,
+  [19, [20, [21, [22, [23, [24, [[[[[25]]]]]]]]]]]
+ ];
+  
+var flatArray = flatten(input);
+console.log(flatArray);
+ // flatArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
