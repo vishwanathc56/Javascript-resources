17c2961c63149a9182c5310d510b8dad738668e3
Vishwanath C <meetvishwa123@rediffmail.com>
2023-06-04 21:38:42 +0530

    Update topics.js

--- JSconf2023/topics.js
+++ JSconf2023/topics.js
@@ -1,1 +1,32 @@
 
+"Yesterday's 𝐉𝐚𝐯𝐚𝐒𝐜𝐫𝐢𝐩𝐭 𝐂𝐨𝐧𝐟𝐞𝐫𝐞𝐧𝐜𝐞 event was an amazing & interesting event! 🚀🎉
+👉 Here are some key points and takeaways from the event :
+
+📌𝐀 𝐓𝐚𝐥𝐞 𝐨𝐟 𝐓𝐰𝐨 𝐖𝐞𝐛𝐬 by Kyle Simpson : He spoke on something new which i never came across i.e. “𝐋𝐨𝐜𝐚𝐥-𝐅𝐢𝐫𝐬𝐭 𝐖𝐞𝐛” where data is stored on local machine rather than a remote server. How client - server causes issues with users and how can it be overcome with this. This was interesting and I need to watch it again and check in more detail.
+
+📌𝐖𝐞𝐛𝐌𝐋: 𝐁𝐫𝐢𝐧𝐠𝐢𝐧𝐠 𝐌𝐚𝐜𝐡𝐢𝐧𝐞 𝐥𝐞𝐚𝐫𝐧𝐢𝐧𝐠 𝐬𝐮𝐩𝐞𝐫𝐩𝐨𝐰𝐞𝐫𝐬 𝐭𝐨 𝐲𝐨𝐮𝐫 𝐉𝐚𝐯𝐚𝐒𝐜𝐫𝐢𝐩𝐭 𝐀𝐩𝐩𝐥𝐢𝐜𝐚𝐭𝐢𝐨𝐧 by Shivay Lamba.  He showed how TensorFlowjs can be used in any JS applications to make it ML based. The ML can be used on client side and also with readily trained models are available.
+
+📌𝐁𝐮𝐢𝐥𝐝𝐢𝐧𝐠 𝐥𝐚𝐫𝐠𝐞 𝐬𝐜𝐚𝐥𝐞 𝐬𝐭𝐚𝐭𝐞𝐟𝐮𝐥 𝐉𝐒 𝐚𝐩𝐩𝐥𝐢𝐜𝐚𝐭𝐢𝐨𝐧𝐬 by Ameer Jhan : He shared some interesting cool stuff on how to use backend services on the front end side without api endpoints. It was interesting and need to be checked.
+
+📌𝐎𝐧 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲 𝐨𝐟 𝐌𝐨𝐝𝐞𝐫𝐧 𝐉𝐚𝐯𝐚𝐒𝐜𝐫𝐢𝐩𝐭 𝐄𝐧𝐠𝐢𝐧𝐞𝐬 (𝐚 𝐇𝐚𝐜𝐤𝐞𝐫'𝐬 𝐏𝐞𝐫𝐬𝐩𝐞𝐜𝐭𝐢𝐯𝐞) by Alisa Esage. She discussed how our code can be vulnerable for exploitation and also browser engines. A must watch
+
+ 📌𝐔𝐧𝐥𝐨𝐜𝐤 𝐭𝐡𝐞 𝐏𝐨𝐰𝐞𝐫 𝐨𝐟 𝐉𝐚𝐯𝐚𝐒𝐜𝐫𝐢𝐩𝐭 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐨𝐫𝐬 by Drishti Jain. How to make best use of generators in our codes(esp in dsa as use case).
+
+📌𝐒𝐭𝐚𝐲𝐢𝐧𝐠 𝐀𝐡𝐞𝐚𝐝 𝐨𝐟 𝐭𝐡𝐞 𝐆𝐚𝐦𝐞: 𝐉𝐚𝐯𝐚𝐒𝐜𝐫𝐢𝐩𝐭 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲 by @Dheeraj Joshi : This is again related to the security part of the application and how our code can be hacked by xSS.
+
+📌𝐁𝐮𝐢𝐥𝐝𝐢𝐧𝐠 𝐈𝐦𝐦𝐞𝐫𝐬𝐢𝐯𝐞 𝐞𝐱𝐩𝐞𝐫𝐢𝐞𝐧𝐜𝐞 𝐨𝐧 𝐭𝐡𝐞 𝐰𝐞𝐛 by Ishan Sharma. Amazing and how can we use three.js and make web immersive/3d experience
+
+ 
+🔹Since two tracks were running simultaneously, I had to switch between rooms based on my interests with topics. So I have shared the topics below(Youtube links and tracks link) which only I attended. Rest which i need to watch since i missed.
+
+🔹I got to know so many things that I'm excited to try out. I also met some really talented developers who shared their insights on the industry. Thanks to everyone"
+
+
+#javascript #conference #jsconf2023 #developers #networking #jsconf #conference #learning #javascriptdevelopers #javascriptdeveloper #JavaScriptConference #reactjs #nodejs
+
+
+https://lnkd.in/gyqzyymb : Jsconf India 2023 - Track 1
+
+https://lnkd.in/gUdGbyH5 : Jsconf India 2023 - Track 2
+
+https://jsconf.in/agenda : For Track 1 & 2
