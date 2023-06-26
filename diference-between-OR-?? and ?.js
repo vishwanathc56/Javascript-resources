a49b6cf61f485600d1c59406fdaa8b51f1cda8cc
Vishwanath C <meetvishwa123@rediffmail.com>
2023-06-26 18:02:00 +0530

    Update diference-between-OR-?? and ?.js

--- diference-between-OR-?? and ?.js
+++ diference-between-OR-?? and ?.js
@@ -1,1 +1,23 @@
 
+In JavaScript ES6, the logical OR operator (`||`), nullish coalescing operator (`??`), and conditional operator (`?`) serve different purposes and are used in different scenarios. Let's break them down:
+
+1. Logical OR Operator (`||`):
+   The logical OR operator (`||`) is used for boolean logic and short-circuit evaluation. It returns the first truthy value encountered or the last value if none of the operands are truthy. Here are a few use cases:
+   
+   const defaultValue = someValue || 'Default'; // Use 'Default' if someValue is falsy
+   const result = value1 || value2 || value3; // Use the first truthy value among value1, value2, and value3
+  
+
+2. Nullish Coalescing Operator (`??`):
+   The nullish coalescing operator (`??`) is used to provide a default value when the value on the left-hand side is `null` or `undefined`. It is specifically designed to handle nullish values and not other falsy values like `0`, `''`, or `false`. Here's an example:
+   
+   const defaultValue = someValue ?? 'Default'; // Use 'Default' if someValue is null or undefined
+  
+
+3. Conditional Operator (`?`):
+   The conditional operator (`?`) is a ternary operator that provides a concise way to write conditional statements. It evaluates a condition and returns one of two expressions based on the result. It's often used as a shorthand for if-else statements. Here's an example:
+   
+   const result = condition ? expressionIfTrue : expressionIfFalse; // Use expressionIfTrue if the condition is truthy, otherwise use expressionIfFalse
+  
+
+To summarize, use `||` for boolean logic and obtaining a default value among multiple options. Use `??` when you want to provide a default value specifically for `null` or `undefined`. Use `?` as a shorthand for conditional statements to choose between two expressions based on a condition.
