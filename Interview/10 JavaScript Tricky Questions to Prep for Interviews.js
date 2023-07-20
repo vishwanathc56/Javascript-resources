5a8ea9f733c14dd093a990e38659536ff107c928
Vishwanath C <meetvishwa123@rediffmail.com>
2023-07-20 21:19:48 +0530

    Update 10 JavaScript Tricky Questions to Prep for Interviews.js

--- Interview/10 JavaScript Tricky Questions to Prep for Interviews.js
+++ Interview/10 JavaScript Tricky Questions to Prep for Interviews.js
@@ -1,4 +1,4 @@
-
+https://medium.com/@kalkidant/10-javascript-tricky-questions-to-prep-for-interviews-e78d18f8226d
 //------------------------------------------------------------------------------------------------------------
 10. What is the output of this code?
   let array = [{channelName:"letitbe"}, "js", 0,1];
