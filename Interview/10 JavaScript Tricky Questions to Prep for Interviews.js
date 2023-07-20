571d749ac06b254d4ac57cc56b4df08c39a178db
Vishwanath C <meetvishwa123@rediffmail.com>
2023-07-20 21:19:36 +0530

    Update 10 JavaScript Tricky Questions to Prep for Interviews.js

--- Interview/10 JavaScript Tricky Questions to Prep for Interviews.js
+++ Interview/10 JavaScript Tricky Questions to Prep for Interviews.js
@@ -1,1 +1,7 @@
 
+//------------------------------------------------------------------------------------------------------------
+10. What is the output of this code?
+  let array = [{channelName:"letitbe"}, "js", 0,1];
+  del array[1];
+  console.log(array.length, "lebn")
+ans: SyntaxError: Unexpected identifier
