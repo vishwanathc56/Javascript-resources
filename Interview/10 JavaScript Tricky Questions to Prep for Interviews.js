07f387048e7fb3382d4c53349954055cadc0e9e1
Vishwanath C <meetvishwa123@rediffmail.com>
2023-07-20 22:10:06 +0530

    Update 10 JavaScript Tricky Questions to Prep for Interviews.js

--- Interview/10 JavaScript Tricky Questions to Prep for Interviews.js
+++ Interview/10 JavaScript Tricky Questions to Prep for Interviews.js
@@ -5,3 +5,17 @@ https://medium.com/@kalkidant/10-javascript-tricky-questions-to-prep-for-intervi
   del array[1];
   console.log(array.length, "lebn")
 ans: SyntaxError: Unexpected identifier
+//------------------------------------------------------------------------------------------------------------
+9. What is the output of this code?
+  obj : {
+    console.log(hello);
+  }
+a. ReferenceError: obj is not defined
+
+b. Syntax error
+
+c. hello
+
+d. ReferenceError: hello is not defined
+ans : ReferenceError: hello is not defined
+//------------------------------------------------------------------------------------------------------------
