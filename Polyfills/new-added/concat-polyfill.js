5a9fb23a2b800742873fd7f82e37f71433e75090
Vishwanath C <meetvishwa123@rediffmail.com>
2023-07-23 01:08:14 +0530

    Update concat-polyfill.js

--- Polyfills/new-added/concat-polyfill.js
+++ Polyfills/new-added/concat-polyfill.js
@@ -29,3 +29,10 @@ Array.prototype.polyfillConcat = function() {
 
   return resultArray;
 };
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5];
+const array3 = [6];
+const concatenatedArray = array1.polyfillConcat(array2, array3, 7, [8, 9]);
+
+console.log(concatenatedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
