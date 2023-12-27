97bbea788cc5a11b50c6601526d4f2871e946eb7
Vishwanath C <meetvishwa123@rediffmail.com>
2023-12-27 18:43:02 +0530

    Update find.js

--- Polyfills/find.js
+++ Polyfills/find.js
@@ -1,17 +1,11 @@
 
-// Check if Array.prototype.find exists
 if (!Array.prototype.find) {
-
-  // If it does not exist, add it to the prototype
   Array.prototype.find = function(callback) {
 
     // Loop through each element of the array
     for (var i = 0; i < this.length; i++) {
 
-      // Call the callback function for each element with the element, index, and array as arguments
       if (callback(this[i], i, this)) {
-
-        // If the callback function returns true, return the current element
         return this[i];
       }
     }
@@ -28,20 +22,5 @@ var found = arr.find(function(element) {
 });
 
 console.log(found); // 4
-
-
-The if statement at the beginning of the code checks if Array.prototype.find exists. If it does not exist, the code inside the if statement will be executed.
-
-Inside the if statement, we define the Array.prototype.find function. This function takes a single argument, callback, which is a function that will be called on each element of the array.
-
-The for loop inside the Array.prototype.find function loops through each element of the array using the this keyword, which refers to the array being operated on.
-
-Inside the loop, the callback function is called for each element of the array using the current element, index, and array as arguments. If the callback function returns true for any element, that element is returned immediately using the return statement. This means that the loop will stop iterating through the array and the Array.prototype.find function will return the first element that satisfies the condition specified in the callback function.
-
-If no element satisfies the condition specified in the callback function, the for loop will complete and the Array.prototype.find function will return undefined.
-
-Finally, the Array.prototype.find function is added to the Array prototype if it doesn't already exist, ensuring that the find function is available for use on all arrays.
-
-
 
 
