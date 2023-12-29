27481161c033556674f74e1401599ec7558adf0e
Vishwanath C <meetvishwa123@rediffmail.com>
2023-12-29 21:04:41 +0530

    Update pop.js

--- Polyfills/pop.js
+++ Polyfills/pop.js
@@ -19,12 +19,3 @@ console.log(popped);
 // prints [1,2]
 console.log(array);
 
-Explanation:
-
-The if statement checks if the pop() method does not already exist on the Array.prototype object. If it doesn't, we define it using a function expression.
-The function first checks if the array is empty. If it is, it returns undefined as specified by the ECMAScript specification.
-The index of the last element of the array is saved to a variable lastIndex.
-The last element of the array is saved to a variable lastItem.
-The last element of the array is deleted from the array using the delete operator.
-The length of the array is decreased by one, and the saved lastItem is returned.
-Note: This implementation assumes that the this keyword refers to a valid array object, and may not work as expected if used on other types of objects.
