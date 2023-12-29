1a5a0aa6f02080aef5a3614e626d2607e5563166
Vishwanath C <meetvishwa123@rediffmail.com>
2023-12-29 21:03:41 +0530

    Update every.js

--- Polyfills/every.js
+++ Polyfills/every.js
@@ -15,10 +15,3 @@ const array = [2, 4, 6, 8];
 // expected output: true
 console.log(array.every(even));
 
-The if statement checks if the every method already exists on the Array.prototype. If it doesn't, we define the every method.
-
-Inside the every method, we take two arguments: callback and thisArg. The callback argument is the function to test each element of the array, and the thisArg argument is the value to use as this when executing the callback.
-
-We then use a for loop to iterate over each element of the array. For each element, we call the callback function with three arguments: the current element, its index, and the original array. If the callback function returns false, we immediately return false, indicating that not all elements passed the test.
-
-If the callback function returns true for all elements, we return true.
