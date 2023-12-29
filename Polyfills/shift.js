35c54721c068380d6d410db9f3233be0bf8aab57
Vishwanath C <meetvishwa123@rediffmail.com>
2023-12-29 21:05:18 +0530

    Update shift.js

--- Polyfills/shift.js
+++ Polyfills/shift.js
@@ -1,3 +1,4 @@
+```
 if (!Array.prototype.shift) {
   Array.prototype.shift = function() {
     if (this.length === 0) return undefined;
@@ -9,7 +10,7 @@ if (!Array.prototype.shift) {
     return firstItem;
   };
 }
-
+```
 const array = [1, 2, 3];
 const firstElement = array.shift(); 
 
@@ -19,11 +20,3 @@ console.log(firstElement);
 // prints [2,3]
 console.log(array);
 
-Explanation:
-
-The if statement checks if the shift() method does not already exist on the Array.prototype object. If it doesn't, we define it using a function expression.
-The function first checks if the array is empty. If it is, it returns undefined as specified by the ECMAScript specification.
-The first element of the array is saved to a variable firstItem.
-The function then loops through the array, starting from the second element, and copies each element to the previous index (i.e., this[i - 1] = this[i]), effectively shifting the elements one index to the left.
-Finally, the length of the array is decreased by one, and the saved firstItem is returned.
-Note: This implementation assumes that the this keyword refers to a valid array object, and may not work as expected if used on other types of objects.
