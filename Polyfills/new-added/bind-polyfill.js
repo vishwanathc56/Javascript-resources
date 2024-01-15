12fc7e66ee900fd0e965f10d86d08a6d640e8275
Vishwanath C <meetvishwa123@rediffmail.com>
2024-01-15 13:19:09 +0530

    Update bind-polyfill.js

--- Polyfills/new-added/bind-polyfill.js
+++ Polyfills/new-added/bind-polyfill.js
@@ -1,10 +1,35 @@
-// Custom implementation of the bind function
+```
+let obj = {
+  name: 'Jack',
+};
+
+let myFunc = function (id, city) {
+  console.log(`${this.name}, ${id}, ${city}`);  // id will be undefined
+};
+
+// Accepting any number of arguments passed to myBind
+Function.prototype.myBind = function (obj, ...args) {
+  let func = this;
+  // Accepting arguments passed to newFunc
+  return function (...newArgs) {
+    func.apply(obj, [...args, ...newArgs]);
+  };
+};
+```
+let newFunc = myFunc.myBind(obj, 'a_random_id')
+newFunc('New York') // Jack, a_random_id, New York
+
+
+
+//------------------------------------------------------------------------------------------
+
+```
 function customBind(fn, context, ...args) {
   return function(...args2) {
     return fn.apply(context, [...args, ...args2]);
   };
 }
-
+```
 // Example usage:
 const obj = {
   name: "John",
